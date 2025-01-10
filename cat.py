24eba0c1fe01bea25055e334e959edf514f3ccde
Princiya1990 <princia.m5@gmail.com>
2025-01-10 20:40:18 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -13,9 +13,11 @@ def adjust_gamma(image, gamma=1.0):
     invGamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
     return cv2.LUT(image, table)
+
 input_folder = "input_images"  # Replace with your input folder path
 output_folder = "output_images"  # Replace with your output folder path
 os.makedirs(output_folder, exist_ok=True)
+
 for filename in os.listdir(input_folder):
     if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff', '.tif')):  
         input_image_path = os.path.join(input_folder, filename)
@@ -24,11 +26,8 @@ for filename in os.listdir(input_folder):
         if image_array is not None:  
             # Apply the CAT process
             img_blend = render(image_array)
-            img_gamma = adjust_gamma(img_blend, gamma=.001)
+            img_gamma = adjust_gamma(img_blend, gamma=0.001)
             cv2.imwrite(output_image_path, img_gamma)
             print(f"Processed image saved: {output_image_path}")
         else:
             print(f"Failed to process image: {input_image_path}")
-
-
-print("Batch processing complete!")
