0c5b4fc7e5edb08900484d44e382d95d0f5b0188
Princiya1990 <princia.m5@gmail.com>
2025-01-21 19:31:03 +0530

    Delete imagetranslatormodule/code/rao.sh

--- imagetranslatormodule/code/rao.sh
+++
@@ -1,1 +0,0 @@
-python3.6 train.py --dataroot /running/CUHK_CV/face2sketch --name facades_pix2pix --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0 --gpu_ids 0,1
