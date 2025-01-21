d1b660b1e27d4388d9c6863cbbbad26c1a452d71
Princiya1990 <princia.m5@gmail.com>
2025-01-21 19:31:27 +0530

    Delete imagetranslatormodule/code/ts.sh

--- imagetranslatormodule/code/ts.sh
+++
@@ -1,5 +0,0 @@
-#python3.6 train.py --dataroot /running/CUHK_CV/face2sketch --name facades_pix2pix --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0 --gpu_ids -1
-#python3.6 test.py --dataroot /running/CUHK_CV/face2sketch --name facades_pix2pix --model pix2pix --direction BtoA --gpu_ids -1
-
-python3.6 test.py --dataroot /running/Test/face2sketch --name facades_pix2pix --model pix2pix --direction BtoA --gpu_ids -1
-
