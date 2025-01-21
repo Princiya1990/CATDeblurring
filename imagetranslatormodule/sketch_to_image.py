492fa4cdadcbfe202be6241070287e965fe93692
Princiya1990 <princia.m5@gmail.com>
2025-01-21 19:50:58 +0530

    Update sketch_to_image.py

--- imagetranslatormodule/sketch_to_image.py
+++ imagetranslatormodule/sketch_to_image.py
@@ -2,9 +2,9 @@ import os
 os.chdir("code")
 combine="python combine_A_and_B.py --fold_A ./../data/CUHK/photos --fold_B ./../data/CUHK/photos --fold_AB ./../data/CUHK/face2sketch"
 os.system(combine)
-train = "python train.py --dataroot ./../data/CUHK --model pix2pix --direction BtoA"
+train = "python train.py --dataroot ./../data/CUHK --name results --model pix2pix --direction BtoA"
 # Execute the command
 os.system(train)
-test = "python train.py --dataroot ./../data/CUHK --model pix2pix --direction BtoA"
+test = "python train.py --dataroot ./../data/CUHK --name results --model pix2pix --direction BtoA"
 # Execute the command
 os.system(test)
