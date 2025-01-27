e7789e8e9d4cf76f004a8129ffa62b63b1f524d9
Princiya1990 <princia.m5@gmail.com>
2025-01-27 09:28:31 +0530

    Update train.py

--- deblurringmodule/train.py
+++ deblurringmodule/train.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
 	# python train.py --dataroot /.path_to_your_data --learn_residual --resize_or_crop crop --fineSize CROP_SIZE (we used 256)
 
 	opt = TrainOptions().parse()
-	opt.dataroot = '/content/DeblurGAN/path/to/data/AB'
+	opt.dataroot = '/content/DeblurGAN/blur2clear'
 	opt.learn_residual = True
 	opt.resize_or_crop = "crop"
 	opt.fineSize = 256
